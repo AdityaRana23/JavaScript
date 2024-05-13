a1a5c862c67ca5619bfc2545efaa0eca5401321e
AdityaRana23 <103068515+AdityaRana23@users.noreply.github.com>
2024-05-13 00:15:49 +0000

    datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+// console.log("Hii, how are you?");
+
+const accountID = 43267
+let accountEmail = "adityrana8744@gmail.com"
+var accountPassword = "12345"
+accountCity = "Windsor"
+let accountState;
+
+// accountID = 23  // not allowed
+
+accountEmail = "jdkhwj@gmail.com"
+accountPassword = "8985"
+accountCity = "Toronto"
+
+/* Prefer not to use var, bcz of issue in block scope and functional scope..*/
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
