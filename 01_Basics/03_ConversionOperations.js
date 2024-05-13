560180aa7e02eeb6cdcc64d0af2320f94f0de04b
AdityaRana23 <103068515+AdityaRana23@users.noreply.github.com>
2024-05-13 01:22:10 +0000

    Datatype Conversion

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)   // Number converts string to number...
 let isLoggedIn = "   ";
 
 let loggedIn = Boolean(isLoggedIn);
-console.log(loggedIn);
+//console.log(loggedIn);
 
 // 1 => true; 0 => false
 // "" => false, "aaditya" => true
@@ -23,5 +23,33 @@ let someNumber = 23
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);  // 23
-console.log(typeof stringNumber); // string
+//console.log(stringNumber);  // 23
+//console.log(typeof stringNumber); // string
+
+// ****************** Operations *********************
+
+let value = 4
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "Hello"
+let str2 = " Aaditya"
+
+let str = str1 + str2
+// console.log(str);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+""); // + se type conversion ho jata h, but we don't have to use it....
