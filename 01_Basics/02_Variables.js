d3bb8181c4bda76993dfc190207c35cd8b27a69d
AdityaRana23 <103068515+AdityaRana23@users.noreply.github.com>
2024-05-13 00:08:53 +0000

    datatypes

---
+++ 01_Basics/02_Variables.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version..
+
+// alert("Hello")  // we are using nodejs, not browser  
+
+//  Code readability should be high always..
+
+console.log("Vishwajeet Singh");
+
+// console.log(5+
+//     9    // It will work but we should write like this, as it reduces the code readibility..
+// );
+
+let name = "Aaditya"
+let age =  19
+let isLoggedIn = false
+let state;
+
+// number(int) => memory => 2 to power 53
+// bigint
+// string => we can either write within single or double quotation
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
